b3e22e9e1cfe8c6ccb9465de03683db34171e10a
Sankalp Dhupar <dhuparsankalp@gmail.com>
2024-09-26 10:52:59 +0000

    Movies List Updated

--- api/neo4j.py
+++ api/neo4j.py
@@ -1,7 +1,7 @@
 from flask import Flask, current_app
+from neo4j import GraphDatabase
 
 # tag::import[]
-from neo4j import GraphDatabase
 # end::import[]
 
 """
@@ -10,9 +10,10 @@ Initiate the Neo4j Driver
 # tag::initDriver[]
 def init_driver(uri, username, password):
     # TODO: Create an instance of the driver here
-    current_app.driver = None
+    current_app.driver = GraphDatabase.driver(uri,auth=(username,password))
+    current_app.driver.verify_connectivity()
 
-    return None
+    return current_app.driver
 # end::initDriver[]
 
 
