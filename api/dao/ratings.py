8191d2f927879034dc01dc813782dc7bfc97e608
Sankalp Dhupar <dhuparsankalp@gmail.com>
2024-09-27 17:07:46 +0000

    Ratings

--- api/dao/ratings.py
+++ api/dao/ratings.py
@@ -18,14 +18,24 @@ class RatingDAO:
     """
     # tag::add[]
     def add(self, user_id, movie_id, rating):
-        # TODO: Create function to save the rating in the database
-        # TODO: Call the function within a write transaction
-        # TODO: Return movie details along with a rating
+        def create_rating(tx,user_id,movie_id,rating):
+            query = """
+                    MATCH (m:Movie {tmdbId: $movie_id})
+                    MERGE (u)-[r:RATED]->(m)
+                    SET r.rating = $rating,
+                        r.timestamp = timestamp()
+                    RETURN m {
+                        .*,
+                        rating: r.rating
+                    } AS movie """
+            res = tx.run(query,movie_id=movie_id,rating=rating,user_id=user_id).single()
+            return res
+        with self.driver.session() as session:
+            record = session.execute_write(create_rating,user_id=user_id,movie_id=movie_id,rating=rating)
+            if record is None:
+                return NotFoundException()
 
-        return {
-            **goodfellas,
-            "rating": rating
-        }
+            return record["movie"]
     # end::add[]
 
 
